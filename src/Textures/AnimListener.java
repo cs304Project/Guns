0ffe0adbbbed01f686e676683ec8db753077ada6
Ze0-Hossam <Hossammohame769@gmail.com>
2022-12-14 23:56:02 +0200

    i attached all file with opengl library to draw your mission ,added an enemy file and added a key system

---
+++ src/Textures/AnimListener.java
@@ -0,0 +1,12 @@
+
+package Textures;
+
+import javax.media.opengl.GLEventListener;
+
+
+
+public abstract class AnimListener implements GLEventListener {
+ 
+    protected String assetsFolderName = "Assets";
+    
+}
---
+++ src/main/Keys/HandleKeys.java
@@ -0,0 +1,44 @@
+
+package Main.Keys;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.BitSet;
+
+/**
+ *
+ * @author hossa
+ */
+public class HandleKeys implements KeyListener {
+    
+    public final int UP = KeyEvent.VK_UP;
+    public final int DOWN = KeyEvent.VK_DOWN;
+    public final int LEFT = KeyEvent.VK_LEFT;
+    public final int RIGHT = KeyEvent.VK_RIGHT;
+       
+    
+    final BitSet keyBits = new BitSet(256);
+    
+    @Override
+    public void keyTyped( final KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(final KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        keyBits.set(keyCode);
+       
+        
+    }
+
+    @Override
+    public void keyReleased(final KeyEvent e) {
+       int keyCode = e.getKeyCode();
+       
+        keyBits.clear(keyCode);
+    }
+    
+    public boolean isKeyPressed(final int keyCode) {
+        return keyBits.get(keyCode);
+    }
+}
